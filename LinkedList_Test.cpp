87431e89165fddf6d8a10b8b916409a86453ecd0
fucker <1004wayne@gmail.com>
2020-09-22 17:45:20 +0800

    updated

---
+++ LinkedList_Test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Linked_List.h"
+int main()
+{
+    LinkedList list;
+    list.Push_front(8);
+    list.Push_front(7);
+    list.Push_front(2);
+    list.Push_back(7);
+    list.PrintList();
+    list.Reverse();
+    list.PrintList();
+    return 0;
+}
